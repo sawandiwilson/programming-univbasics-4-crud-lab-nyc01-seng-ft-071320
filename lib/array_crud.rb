641f311834ff33d935e19aa5e8187b4e6aa0b244
Sawandi Wilson <sawandiwilson@gmail.com>
2020-07-01 19:58:53 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -28,11 +28,11 @@ def retrieve_element_from_index(array, index_number)
 end
 
 def retrieve_first_element_from_array(array)
-  
+  "wow"
 end
 
 def retrieve_last_element_from_array(array)
-  "wow"
+  
 end
 
 def update_element_from_index(array, index_number, element)
